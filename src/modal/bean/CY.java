ebba5604b92c85bec4a20b9b0459d703aeaf9d42
SunMoonStarts <634214953@qq.com>
2018-06-11 17:39:24 +0800

    xiugai lei

--- src/modal/bean/CY.java
+++ src/modal/bean/CY.java
@@ -3,8 +3,17 @@ package modal.bean;
 public class CY {
 	
 	private String name;
-private String name11;
-public void UTF() {
-	System.out.println("希望不要有乱码aaaaaaaa111111");
-}
+	
+	private String name11;
+	
+	public void UTF() {
+		
+		System.out.println("希望不要有乱码aaaaaaaa111111");
+		
+	}
+public void UTF2() {
+		
+		System.out.println("希望不要有乱码aaaaaaaa111111");
+		
+	}
 }
